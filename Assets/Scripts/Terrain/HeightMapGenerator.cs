33c8f7b8bbbe65b09a1840a2cd02cd511570fb41
Nick Maltbie <nick.dmalt@gmail.com>
2019-11-04 23:38:40 -0500

    Added duplicate of basic heightmap and mesh code

---
+++ Assets/Scripts/Terrain/HeightMapGenerator.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class HeightMapGenerator : MonoBehaviour {
+    public int seed;
+    public bool randomizeSeed;
+
+    public int numOctaves = 7;
+    public float persistence = .5f;
+    public float lacunarity = 2;
+    public float initialScale = 2;
+
+    public float[] Generate (int mapSize) {
+        var map = new float[mapSize * mapSize];
+        seed = (randomizeSeed) ? Random.Range (-10000, 10000) : seed;
+        var prng = new System.Random (seed);
+
+        Vector2[] offsets = new Vector2[numOctaves];
+        for (int i = 0; i < numOctaves; i++) {
+            offsets[i] = new Vector2 (prng.Next (-1000, 1000), prng.Next (-1000, 1000));
+        }
+
+        float minValue = float.MaxValue;
+        float maxValue = float.MinValue;
+
+        for (int y = 0; y < mapSize; y++) {
+            for (int x = 0; x < mapSize; x++) {
+                float noiseValue = 0;
+                float scale = initialScale;
+                float weight = 1;
+                for (int i = 0; i < numOctaves; i++) {
+                    Vector2 p = offsets[i] + new Vector2 (x / (float) mapSize, y / (float) mapSize) * scale;
+                    noiseValue += Mathf.PerlinNoise (p.x, p.y) * weight;
+                    weight *= persistence;
+                    scale *= lacunarity;
+                }
+                map[y * mapSize + x] = noiseValue;
+                minValue = Mathf.Min (noiseValue, minValue);
+                maxValue = Mathf.Max (noiseValue, maxValue);
+            }
+        }
+
+        // Normalize
+        if (maxValue != minValue) {
+            for (int i = 0; i < map.Length; i++) {
+                map[i] = (map[i] - minValue) / (maxValue - minValue);
+            }
+        }
+
+        return map;
+    }
+}
